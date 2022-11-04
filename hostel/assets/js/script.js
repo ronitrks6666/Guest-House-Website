c4bdac9794623179a2cf11189013a6208f74c09a
ronitrks6666 <ronitrks6666@gmail.com>
2022-11-04 11:07:46 +0530

    Inital Commit

---
+++ hostel/assets/js/script.js
@@ -0,0 +1,8 @@
+$("img").click(function () {
+    $("#full-image").attr("src", $(this).attr("src"));
+    $('#image-viewer').show();
+});
+
+$("#image-viewer .close").click(function () {
+    $('#image-viewer').hide();
+});
